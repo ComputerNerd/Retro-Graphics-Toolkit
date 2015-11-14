f4b5aee371f538b5c0dd3c2577a0248f5d304614
ComputerNerd <theprogrammernerd@gmail.com>
2015-11-14 15:09:05 -0600

    Made internal references to other chapters in the manual hyperrefs

--- Manual/genManuals.py
+++ Manual/genManuals.py
@@ -7,6 +7,14 @@ def toGithub(inmd,outmd):
     print('Converting to github markdown '+inmd)
     if subprocess.call(['pandoc','-S','-f','markdown','-t','markdown_github','-o',outmd,inmd]):
         print('pandoc failure')
+    f = open(outmd,'r+')
+    txt=f.read()
+    txt=txt.replace('\\[\\[','[[')
+    txt=txt.replace('\\]\\]',']]')
+    f.seek(0)
+    f.write(txt)
+    f.truncate()
+    f.close()
 import os
 import glob
 import subprocess
@@ -39,22 +47,23 @@ if len(sys.argv)<=1:
     for name in sorted(glob.glob('Pandoc/*.tex')):
         if 'Pandoc/Home.tex'!=name:
             basename=os.path.basename(os.path.splitext(name)[0])
-            basename=basename.replace('-',' ')
-            concat+='\chapter{'+basename+'}\n'
+            concat+='\\chapter{'+basename.replace('-',' ')+'}\n'
+            concat+='\\label{'+basename+'}\n'
             concat += open(name).read()
     concat += open('footer.tex').read()
     concat=concat.replace('includegraphics{https://raw.githubusercontent.com/ComputerNerd/Retro-Graphics-Toolkit/master/','includegraphics{../')
-    i=0
     while True:
-        fs=[m.start() for m in re.finditer('{\\[}{\\[}',concat)]
-        fm=[m.start() for m in re.finditer('\\textbar{}',concat)]
-        fe=[m.start() for m in re.finditer('{\\]}{\\]}',concat)]
-        if len(found)-1>=i:
+        fs=concat.find('{[}{[}')
+        fm=concat.find('\\textbar{}')
+        fe=concat.find('{]}{]}')
+        if fs<0:
             break
-        before=concat[0:fs[i]]
-        after=concat[fs[i]+6:]
-        after=after.replace('\\textbar{}',
-        i+=1
+        labelText=concat[fs+6:fm]
+        labelName=concat[fm+10:fe]
+        before=concat[0:fs]
+        after=concat[fe+6:]
+        print(labelText,labelName)
+        concat=before+'\\hyperref['+labelName+']{'+labelText+'}'+after
     with open("Manual.tex",'w') as f:
         f.write(concat)
     args=['xelatex','Manual']
