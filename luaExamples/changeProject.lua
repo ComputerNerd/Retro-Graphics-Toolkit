94814c8fb5a805d8a1c6a408c8157bf0a2456262
ComputerNerd <theprogrammernerd@gmail.com>
2018-12-08 20:12:33 -0600

    Show an alert when the user tries to run the change project example with only one project. The change project example requires two or more projects to be useful.

--- luaExamples/changeProject.lua
+++ luaExamples/changeProject.lua
@@ -1,10 +1,15 @@
 -- Changes the project based on ID.
-set=tonumber(fl.input(string.format("Current project ID is: %d\nEnter the project ID you want to switch to from [1,%d]",projects.currentIdx, project.count)))
-if set~=nil then
-	if project.set(set)==true then
-		fl.alert(string.format("Project set to %d", projects.currentIdx))
-	else
-		fl.alert(string.format("ID %d is out of bounds or is already set to %d",set,set))
+if #projects < 2 then
+	fl.alert('This example requires a minimum of two projects.')
+	return
+else
+	set=tonumber(fl.input(string.format("Current project ID is: %d\nEnter the project ID you want to switch to from [1,%d].",projects.currentIdx, project.count)))
+	if set~=nil then
+		if project.set(set)==true then
+			fl.alert(string.format("Project set to %d", projects.currentIdx))
+		else
+			fl.alert(string.format("ID %d is out of bounds or is already set to %d",set,set))
+		end
 	end
+	rgt.redraw()
 end
-rgt.redraw()
