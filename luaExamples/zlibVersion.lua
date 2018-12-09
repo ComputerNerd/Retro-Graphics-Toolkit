24f7f52152b33a8b362e36f89501654f1bf94403
ComputerNerd <theprogrammernerd@gmail.com>
2018-12-08 20:09:29 -0600

    Use fl.message instead of print and show the minor and patch in addition to the major version.

--- luaExamples/zlibVersion.lua
+++ luaExamples/zlibVersion.lua
@@ -1,3 +1,4 @@
 -- Retro Graphics Toolkit is compiled with the Zlib Lua binding from https://github.com/brimworks/lua-zlib
 -- That is where you will find documentation for The Zlib binding.
-print(zlib.version())
+major, minor, patch = zlib.version()
+fl.message(string.format('%d.%d.%d', major, minor, patch))
